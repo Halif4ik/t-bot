f5f38c5e4b5535ff1bc38692719d41777b03ef99
DimaGrimach <chiki.briki2@gmail.com>
2024-02-27 14:47:59 +0200

    implement delete messages

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# t-bot
+# Telegram Bot with Telegraf
+
+This is a simple Telegram bot created using the Telegraf library in Node.js. The bot forwards incoming messages to a
+specific user or chat.
+
+## Prerequisites
+
+Before running the bot, ensure you have the following installed on your machine:
+
+- Node.js
+- npm or Yarn
+- TypeScript (if you're using TypeScript)
+
+## Getting Started
+
+1. Clone this repository to your local machine.
+
+2. Install dependencies using npm or Yarn:
+
+   ```bash
+   npm install
+   # or
+   yarn install
+    ```
+3. Create a .env file in the root directory of the project and add the following environment variables:
+   TELEGRAM_BOT_TOKEN=YOUR_TELEGRAM_BOT_TOKEN
+   CHAT_ID=TARGET_CHAT_ID
+4. Running the Bot
+   To run the bot, execute the following command:
+
+```bash
+Copy code
+npm run dev
+# or
+yarn dev
+```
+The bot should now be running and forwarding incoming messages to the specified chat.
+
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,75 @@
-import {Telegraf} from 'telegraf';
+import {Markup, session, Telegraf} from 'telegraf';
+import {ConfigService} from "./config/config.service";
+import {ICtxInterface} from "./ctx.interface.js";
 
+const configService: ConfigService = new ConfigService();
+const bot: Telegraf<ICtxInterface> = new Telegraf(configService.get('MY_TOKEN'), {});
 
-// Initialize your bot with your Telegram Bot API token
-const bot = new Telegraf(myToken,{});
-
+bot.use(session())
 // Middleware to forward incoming messages to a specific user or chat
-bot.use(async (ctx, next) => {
+bot.use(async (ctx: ICtxInterface, next): Promise<void> => {
    try {
-      // Forward the incoming message to a specific user or chat
-      await ctx.telegram.forwardMessage(chatId, ctx.from.id, ctx.message.message_id);
-      console.log('Message forwarded successfully');
+      if (ctx.from && ctx.message && ctx.chat) {
+         const botForwMassID = await ctx.telegram.forwardMessage(configService.get('CHAT_ID'), ctx.from.id,
+             ctx.message.message_id);
+
+         if (!ctx.session) ctx.session = {
+            CTAButton: {other: false},
+            messageFromUserInfo: {
+               chatId: ctx.chat.id ?? 0,
+               messageId: 0
+            },
+            buttonMessageId: 0,
+            botForwardedMessageId: botForwMassID.message_id,
+         };
+         ctx.session.botForwardedMessageId = botForwMassID.message_id,
+
+             console.log('Message forwarded successfully');
+      } else {
+         console.log('ctx.from and ctx.message is undefined');
+      }
    } catch (error) {
       console.error('Error forwarding message:', error);
    }
-   // Continue handling other middleware or actions
-   next();
+   await next();
 });
 
-// Start listening for incoming messages
+// Attach buttons to messages and store messages information in session
+bot.on('text', async (ctx: ICtxInterface): Promise<void> => {
+   if (!ctx.session || !ctx.chat || !ctx.message || !ctx.chat.id || !ctx.message.message_id) {
+      console.error('Chat id or message id is undefined');
+      return;
+   }
+
+   ctx.session.messageFromUserInfo = {
+      chatId: ctx.chat.id,
+      messageId: ctx.message.message_id
+   };
+   const keyboardMarkup = Markup.inlineKeyboard([
+      Markup.button.callback('Other', 'other'),
+      Markup.button.callback('Add', 'add')
+   ]);
+
+   const message = await ctx.reply('Vars answer', keyboardMarkup);
+   ctx.session.buttonMessageId = message.message_id;
+});
+
+// Handle button actions Clear all messages in the chat
+bot.action('other', async (ctx: ICtxInterface): Promise<void> => {
+   try {
+      if (ctx.session && ctx.session.messageFromUserInfo && ctx.session.messageFromUserInfo.chatId && ctx.session.messageFromUserInfo.messageId) {
+         await ctx.telegram.deleteMessage(ctx.session.messageFromUserInfo.chatId, ctx.session.messageFromUserInfo.messageId);
+         await ctx.telegram.deleteMessage(ctx.session.messageFromUserInfo.chatId, ctx.session.buttonMessageId);
+         await ctx.telegram.deleteMessage(ctx.session.messageFromUserInfo.chatId, ctx.session.botForwardedMessageId);
+         console.log('All messages cleared successfully');
+      }
+   } catch (error) {
+      console.error('Error clearing messages:', error);
+   }
+});
+
+bot.action('add', async (ctx: ICtxInterface) => {
+   // Do nothing for the 'add' button
+});
 bot.launch().then(() => {
-   console.log('Bot started');
 });
